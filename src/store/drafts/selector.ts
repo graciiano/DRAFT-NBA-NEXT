14c89958e18992b40d9dc97862329f31ec540861
diogo.souza <diogo.souza1@consulting-for.edenred.com>
2025-12-16 11:37:53 -0300

    fix

--- src/store/drafts/selector.ts
+++ src/store/drafts/selector.ts
@@ -1,23 +1,25 @@
+import { Draft } from '@/types/api';
 import { createSelector } from '@reduxjs/toolkit';
 
 import { RootState } from '../index';
+import { DraftsState } from './types';
 
 // Base selector
 const selectDraftsState = (state: RootState) => state.drafts;
 
 // Memoized selectors
-export const selectDrafts = createSelector([selectDraftsState], drafts => drafts.drafts);
+export const selectDrafts = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.drafts);
 
-export const selectCurrentDraft = createSelector([selectDraftsState], drafts => drafts.currentDraft);
+export const selectCurrentDraft = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.currentDraft);
 
-export const selectDraftsLoading = createSelector([selectDraftsState], drafts => drafts.loading);
+export const selectDraftsLoading = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.loading);
 
-export const selectDraftsError = createSelector([selectDraftsState], drafts => drafts.error);
+export const selectDraftsError = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.error);
 
-export const selectSignupLoading = createSelector([selectDraftsState], drafts => drafts.signupLoading);
+export const selectSignupLoading = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.signupLoading);
 
-export const selectSignupError = createSelector([selectDraftsState], drafts => drafts.signupError);
+export const selectSignupError = createSelector([selectDraftsState], (drafts: DraftsState) => drafts.signupError);
 
-export const selectOpenDrafts = createSelector([selectDrafts], drafts =>
-  drafts.filter(draft => draft.status === 'OPEN')
+export const selectOpenDrafts = createSelector([selectDrafts], (drafts: Draft[]) =>
+  drafts.filter((draft: Draft) => draft.status === 'OPEN')
 );
